15cdac2c23f9abfa8b4cc0d6f6e58a91276f1c47
koredeowoblow <100287793+koredeowoblow@users.noreply.github.com>
2024-07-12 18:52:26 +0100

    schemas update

---
+++ database/migrations/create_product_table.php
@@ -0,0 +1,39 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Products extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->int('category_id');
+            $table->string('title', 100);
+            $table->decimal('price', 10, 2);
+            $table->text('description')->nullable();
+            $table->boolean('negotiation')->default(false);
+            $table->string('contact', 50);
+            $table->string('email', 100);
+            $table->datetime('created_at');
+            $table->datetime('updated_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
---
+++ database/migrations/create_productattributes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ProductAttributes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_attributes', function (Blueprint $table) {
+            $table->id();
+            $table->int('product_id');
+            $table->int('attribute_id');
+            $table->string('value', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_attributes');
+    }
+}
