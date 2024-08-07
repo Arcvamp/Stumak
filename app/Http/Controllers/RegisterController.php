a6fe533f15b68c2b10d2e2d98a84735a11337750
prihubs <prihubs.prihubs.com>
2024-08-07 21:53:25 +0100

    Updated RegisterController and Route --

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -10,7 +10,10 @@ class RegisterController extends Controller
 {
 
     public function create(){
-        //
+
+        return view('register', [
+            'title' => 'Create Account'
+        ]);
     }
 
     public function store(Request $request){
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('user/signup', [RegisterController::class , 'create']);
+Route::post('user/signup', [RegisterController::class , 'store']);
