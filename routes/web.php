91b67eb9371a00684c56fe0033823eff159d580f
koredeowoblow Owolabi <koredeowolabi62@gmail.com>
2024-09-10 15:22:19 +0000

    view pages

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Route::get('/test', function () {
